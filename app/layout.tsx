481ff2ba21b2dec8b5d53215bfde2757f430c6ae
wiley <bluechanel612@gmail.com>
2024-03-07 13:22:55 +0800

    修改名称并增加app图标获取

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 "use client"
 import React from 'react';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
-import { ClockCircleOutlined, SettingOutlined, PieChartOutlined } from '@ant-design/icons';
-import { Button, Flex, Layout } from 'antd';
+import { ClockCircleOutlined, SettingOutlined, PieChartOutlined, UserOutlined } from '@ant-design/icons';
+import { Avatar, Button, Flex, Layout } from 'antd';
+
+
+
 
 const { Content, Sider } = Layout;
 const RootLayout = ({ children }: React.PropsWithChildren) => (
@@ -11,10 +14,10 @@ const RootLayout = ({ children }: React.PropsWithChildren) => (
       <Layout hasSider>
         <Sider width="50px" theme="light" style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0, top: 0, bottom: 0 }}>
           <Flex gap="middle" vertical className='w-8 h-max' justify="center" align="center">
-            <Button type="primary" shape="circle">icon</Button>
-            <Button type="primary" shape="circle" icon={<ClockCircleOutlined />} href='/timer' />
-            <Button type="primary" shape="circle" icon={<PieChartOutlined />} href='/analyse' />
-            <Button type="primary" shape="circle" icon={<SettingOutlined />} href='/settings'/>
+            <Avatar shape="square" size="large" src="/app-icon.png" />
+            <Button shape="circle" icon={<ClockCircleOutlined />} href='/timer' />
+            <Button shape="circle" icon={<PieChartOutlined />} href='/analyse' />
+            <Button shape="circle" icon={<SettingOutlined />} href='/settings' />
           </Flex>
         </Sider>
         <Content>
